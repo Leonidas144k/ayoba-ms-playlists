eed774e53e14199ed1769b74e565993e440e799d
Leonidas144k <mkmothoa@gmail.com>
2022-07-21 18:52:59 +0200

    Adding Ayoba Playlist Model and MongoDB

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.AyobaPlaylist
+playlists = db.playlists
 
 app = Flask(__name__)
 
+'''
 playlists = [
     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
 ]
 '''
 @app.route('/')
-def index():
-    """Return homepage."""
-    # change the original return statement you wrote to the one below
-    return render_template('home.html', msg='Flask is Cool!!')
-'''
-@app.route('/')
 def playlists_index():
     """Show all playlists."""
-    return render_template('playlists_index.html', playlists=playlists)
+    return render_template('playlists_index.html', playlists=playlists.find())
 
 
 if __name__ == "__main__":
